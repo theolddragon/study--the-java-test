54d7ec4eb813d2bd5c87350192ef549cda887b24
Tigger <tigger@mobidoo.co.kr>
2020-12-28 18:42:40 +0900

    9. JUnit 5: 테스트 반복하기 2부

--- src/main/java/kr/theolddragon/studythejavatest/Study.java
+++ src/main/java/kr/theolddragon/studythejavatest/Study.java
@@ -2,17 +2,23 @@ package kr.theolddragon.studythejavatest;
 
 public class Study {
 
-  private StudyStatus status;
+  private StudyStatus status = StudyStatus.DRAFT;
 
   private int limit;
+
+  private String name;
 
   public Study(int limit) {
     if (limit < 0) {
       throw new IllegalArgumentException("limit은 0보다 커야 한다.");
     }
 
-    this.status = StudyStatus.DRAFT;
     this.limit = limit;
+  }
+
+  public Study(int limit, String name) {
+    this.limit = limit;
+    this.name = name;
   }
 
   public StudyStatus getStatus() {
@@ -21,5 +27,18 @@ public class Study {
 
   public int getLimit() {
     return this.limit;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  @Override
+  public String toString() {
+    return "Study{" +
+        "status=" + status +
+        ", limit=" + limit +
+        ", name='" + name + '\'' +
+        '}';
   }
 }
--- src/test/java/kr/theolddragon/studythejavatest/StudyTest.java
+++ src/test/java/kr/theolddragon/studythejavatest/StudyTest.java
@@ -24,7 +24,19 @@ import org.junit.jupiter.api.condition.EnabledOnJre;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.JRE;
 import org.junit.jupiter.api.condition.OS;
+import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.AggregateWith;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregationException;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregator;
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.converter.SimpleArgumentConverter;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -55,8 +67,48 @@ class StudyTest {
   @DisplayName("스터디 만들기 매개변수")
   @ParameterizedTest(name = "{index} {displayName} message={0}")
   @ValueSource(strings = {"날씨가", "많이", "추워지고", "있네요"})
+  @NullAndEmptySource
   void parameterizedTest(String message) {
     System.out.println(message);
+  }
+
+  @DisplayName("스터디 만들기 매개변수 By ints")
+  @ParameterizedTest(name = "{index} {displayName} message={0}")
+  @ValueSource(ints = {10, 20, 40})
+  void parameterizedTestByInts(@ConvertWith(StudyConverter.class) Study study) {
+    System.out.println(study.getLimit());
+  }
+
+  static class StudyConverter extends SimpleArgumentConverter {
+
+    @Override
+    protected Object convert(Object source, Class<?> targetType) throws ArgumentConversionException {
+      assertEquals(Study.class, targetType, "Can only convert to Study");
+      return new Study(Integer.parseInt(source.toString()));
+    }
+  }
+
+  @DisplayName("스터디 만들기 매개변수 By CSV")
+  @ParameterizedTest(name = "{index} {displayName} message={0}")
+  @CsvSource({"10, '자바 스터디'", "20, '스프링'"})
+  void parameterizedTestByCSV(ArgumentsAccessor argumentsAccessor) {
+    final Study study = new Study(argumentsAccessor.getInteger(0), argumentsAccessor.getString(1));
+    System.out.println(study);
+  }
+
+  @DisplayName("스터디 만들기 매개변수 By CSV And Aggregator")
+  @ParameterizedTest(name = "{index} {displayName} message={0}")
+  @CsvSource({"10, '자바 스터디'", "20, '스프링'"})
+  void parameterizedTestByCSVAndAggregator(@AggregateWith(StudyAggregator.class) Study study) {
+    System.out.println(study);
+  }
+
+  static class StudyAggregator implements ArgumentsAggregator {
+
+    @Override
+    public Object aggregateArguments(ArgumentsAccessor accessor, ParameterContext context) throws ArgumentsAggregationException {
+      return new Study(accessor.getInteger(0), accessor.getString(1));
+    }
   }
 
   @Test
